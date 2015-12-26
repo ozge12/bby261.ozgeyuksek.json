8ac330c85692e346fed12d884c8044c5f4e62c4e
ozge12 <ozge.yuksek12@gmail.com>
2015-12-26 20:30:08 +1200

    start

---
+++ database.js
@@ -0,0 +1,24 @@
+var catalog = [
+	{
+        "itemName":"Mitoloji",
+        "itemAuthor":"Kolektif",
+        "itemImage":" mitoloji.jpg",
+        "itemCreationDate":"2009",
+        "itemDescription":"<p> Her toplumun,dünyanın yaratılışından onun nasıl sona ereceğine kadar çeşitli olayları hikaye eden mitleri vardır.Bu kitapta Eski Mısır tanrılarından Hindistan'ın Veda tanrıların,Maya,İnka ve Aztek efsanelerinden Aborijinlerin Düş Zamanı'na,Babil,Sümer ve Asur destanlarından Yunan ve Roma efsanelerine kadar binyıllardır tüm dünya toplumlarını şekillendiren mitler ve yaratılış hikayeleri yer alıyor.Her hikaye,onu yaratan toplumla ve bu hikayelerin şekillendirdiği,esin kaynağı olduğu insanlar ve hareketlerle birlikte inceliyor. <br> (Sayfa Sayısı: 480 )</p>"
+    }, 
+{
+    
+    "itemName": "Şaman-Ateşin Efendisi" , 
+    "itemAuthor":"Harald Braem",
+        "itemImage":" mitoloji1.jpg",
+        "itemCreationDate":"1999",
+        "itemDescription": "<p> Asya'nın Ayincisi, Ateşin Efendisi Şaman Mitolojisinin RomanıBüyük davulu ve ustasının pelerinini alıyor. Ayinlerde gerekli olan bir sürü ufak tefek nesneyi keselere doldurup beline bağlıyor. Dizlerinin üstüne oturup kürek çekiyor ve emniyetli bir şekilde yavaşça akıntının içinde ilerliyor. Kafasındaki tüylü başlık rüzgarda uçuşuyor. Tayganın kralı Malu'nun gururlu boynuzlarını da taşıyor üstünde; ruhunda ise bir daha asla sönmeyecek olan bir alev dalgalanıyor. Sisin içinde dimdik duruyor ve yavaşça kıyıda onu bekleyen insanlara doğru yol alıyor: Bir kartal, bir geyik ve Ateşin Efendisi. Ve ayaklarının dibine uzanmış bir kurt.Yüce şaman Bo-Han işte bu şekilde aşıyor nehri.<br> (Sayfa sayısı:326 ) </p>"
+    
+    
+    
+    
+    
+    
+    
+}
+];
