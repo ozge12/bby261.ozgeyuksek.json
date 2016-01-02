32b5f55b459fce4c3ddf86e7926ce7af2a424a5d
ozge12 <ozge.yuksek12@gmail.com>
2016-01-02 22:49:26 +1200

    katologlama

--- database.js
+++ database.js
@@ -13,6 +13,89 @@ var catalog = [
         "itemImage":" mitoloji1.jpg",
         "itemCreationDate":"1999",
         "itemDescription": "<p> Asya'nın Ayincisi, Ateşin Efendisi Şaman Mitolojisinin RomanıBüyük davulu ve ustasının pelerinini alıyor. Ayinlerde gerekli olan bir sürü ufak tefek nesneyi keselere doldurup beline bağlıyor. Dizlerinin üstüne oturup kürek çekiyor ve emniyetli bir şekilde yavaşça akıntının içinde ilerliyor. Kafasındaki tüylü başlık rüzgarda uçuşuyor. Tayganın kralı Malu'nun gururlu boynuzlarını da taşıyor üstünde; ruhunda ise bir daha asla sönmeyecek olan bir alev dalgalanıyor. Sisin içinde dimdik duruyor ve yavaşça kıyıda onu bekleyen insanlara doğru yol alıyor: Bir kartal, bir geyik ve Ateşin Efendisi. Ve ayaklarının dibine uzanmış bir kurt.Yüce şaman Bo-Han işte bu şekilde aşıyor nehri.<br> (Sayfa sayısı:326 ) </p>"
+    },
+    
+    
+    {
+        "itemName":"Şarap ve Tanrı",
+        "itemAuthor":"Kamil Sarhanlı",
+        "itemImage":" mitoloji2.jpg",
+        "itemCreationDate":"2014",
+        "itemDescription":"<p> Yunan Mitolojisi'nde tanrıların tanrısı Zeus'un oğlu Dionysos, dile gelip kendi hikâyesini anlatıyor. Şarabın ve eğlencenin tanrısının başından geçenler, tarihsel bilgiyle yoğuruluyor.Şarap ve Tanrı'da yazar yaptığı araştırmayla Dionysos'u edebi bir dille ete kemiğe büründürüyor. <br> (Sayfa Sayısı:152 ) </p> "
+        
+       }, 
+    
+    
+    {
+        "itemName":"Kızılderili Mitolojisi",
+        "itemAuthor":"Alice Marriott, Carol K. Rachlin",
+        "itemTranslotor" : "Ünsal Özünlü" ,
+        "itemImage":" mitoloji3.jpg",
+        "itemCreationDate":"1998",
+        "itemDescription":"<p> Kızılderili Mitolojisi'nde yer alan efsane ve destanların bazıları Yaratılışla ilgilidir. Bunlar dünyanın, güneşin ayın yıldızların ve insanların nasıl oluştuğunu anlatırlar. Bazı öyküler doğaya, bazıları da ölüm ve ölümden sonraki yaşama ilişkindir. Bu efsane ve destanlarda, Kızılderililerin dinleri, gelenekleri, tarihleri, etnik gülmece biçimleri, kabileler arasındaki benzer ve farklı yanlar ve Beyaz Adamın gelişinin neden olduğu değişiklikler ele alınmaktadır.Bu kitapta destan ve efsaneler, günümüzde yaşamakta olan Kızılderililer tarafından kitabın yazarlarına anlatılmış ve doğrulukları bazı antropolojik desteklerle de kanıtlanmıştır. Bu yapıt, antropoloji, edebiyat ve tarih alanlarında çalışmalarda bulunanlar için önemli bir kaynaktır.<br> (Sayfa Sayısı: 294 )</p>"
+    }, 
+    
+    
+    {
+        "itemName":"Beyaz Adam Gelmeden Önce",
+        "itemAuthor":"Egerton R. Young",
+        "itemTranslotor" : "Nur Yener" ,
+        "itemImage":" mitoloji4.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p> Nanahboozhoo kötü ruhlarla uğraşmak zorunda olduğu için çok dikkatli olmalıydı. Değişik kılıklara girdi ve sonunda kır kurdunun yer altı dünyasındaki bekçilerden ateşi nasıl çaldığını duydu. Altımızdaki dünyada bu kadar büyük bir ateşin olması onu biraz korkutmuştu ama uzun süre korkmaya meyilli biri değildi ve araştırmaya başlayıp Windegooların ve diğerlerinin konuşmalarını gizlice dinlerken uzun zamandır aradığı ateşin acımasız yaşlı bir şifacı savaşçının elinde olduğunu öğrendi. Ateşi elde etme işini kurda verenler onu koruma görevini de bu şifacı savaşçıya vermişlerdi. Bu yaşlı savaşçının, ateşi korumasında ona yardım eden iri yarı, amansız iki kızı vardı. O günlerdeki düşünceye göre ateş öyle tehlikeli bir şeydi ki eğer herkese verilirse bütün ırkın ortadan kalkmasına neden olabilirdi. Bundan önce de ateşi çalmak için birkaç girişimde bulunulmuştu. Ama hepsi başarısızlıkla sonuçlanmış ve bunu yapmaya çalışanlar öldürülmüştü. Ancak Nanahboozhoo çok becerikli biriydi ve bu onun ilk büyük teşebbüslerinden biri olacak olsa da korkuları onu durduramayacaktı.(Tanıtım Bülteninden)<br> (Sayfa Sayısı: 176 )</p>"
+    }, 
+    
+    
+     {
+        "itemName":"Denizler Tanrısı-Poseidon",
+        "itemAuthor":"Robert Krugmann",
+        "itemImage":" mitoloji5.jpg",
+        "itemCreationDate":"2003",
+        "itemDescription":"<p>  <center> <br>Üç Dişli Yabasıyla Yerleri Sarsan, <br> Odysseus’a Dönüş Yolunu Dar Eden, <br>Lacivert Saçlı Denizler Tanrısı Poseidon. <center/>  <br> Zeus rakiplerini alt etmeyi başarmış ve onları yeraltı dünyasında tunç kapılı bir zindana tıkmıştı. Artık egemenliğinin önünde hiçbir engel yoktu. Elbette ağabeyleri olan Poseidon ve Hades hariç! Zeus bu iki ağabeyiyle uzun süren pazarlıklar yapmış, sonunda onlara şu anlaşmayı kabul ettirebilmişti: Kâinatın efendisi, bulutların arasındaki göğün sahibi kendisi olacaktı.Yeraltı dünyası Hades’e, denizlerin egemenliği Poseidon’a verilecekti. Bunların üçünün arasında kalan kara parçalarında herkesin payı vardı.Kendisini dünyanın büyük kısmının efendisi olarak gören Poseidon’un hesaba katmadığı bir şey vardı: Ona tapınacak olan insanlar yeryüzünde oturuyordu!<br> (Sayfa Sayısı:62)  </p>"  
+    }, 
+    
+    
+    
+    {
+        "itemName":"Tanrılar Kadınken",
+        "itemAuthor":" Merlin Stone",
+        "itemTranslotor" : "Nilgün Şarman" ,
+        "itemImage":" mitoloji6.jpg",
+        "itemCreationDate":"2000",
+        "itemDescription":"<p> Yiyecek toplama edimlerinin bir uzantısı olarak tarımın gelişmesine kadınların yol açtığını öne süren, herkesin kabul ettiği kurama uygun olarak, uygarlığa bu armağanı bağışlamış kadın tanrılar her yerde görülüyordu. Tarımsal gelişimin ilk kanıtlarının bulunduğu Mezopotamya'da Tanrıça Ninlil, halkına ekim ve hasat yöntemlerini öğrettiği için ululanıyordu. Bazı söylenceler kadını savaşlarda önder; güçlü ve yürekli bir cenkçi olarak tanımlar. Sonraları klasik Yunan edebiyatının Amazonlar adıyla andığı kadın askerlerle ilgili sayısız kayıt, Tanrıçaya yiğit bir savaşçı olarak tapınıldığını göstermektedir. Amazonların kadın tanrılara gösterdiği saygıyla ilgili öyküler daha kapsamlı bir biçimde incelendiğinde, Libya, Anadolu, Bulgaristan, Yunanistan, Ermenistan ve Rusya topraklarında avlanıp savaşan savaşçı tanrıçalara tapınan kadınlar apaçık görülür; ayrıca bunlar günümüz yazarlarının bizi inandırmaya çalıştığı gibi söylence düşlemleri de değildir. Bu kitabın birçok insanı, bu konuyu kendi adına araştırmaya iteceğini umuyorum; böylece belki bir gün geçmişin olaylarını daha iyi anlayabilir, bile bile ya da dikkatsizlik sonucu gizlenen konuları açığa çıkarabilir, olgu diye bakılan temelsiz birçok varsayıma meydan okuyabiliriz. <br> (Sayfa Sayısı:294) </p>"  
+    }, 
+    
+    
+    
+    
+    {
+        "itemName":"Odysseia",
+        "itemAuthor":"Homeros",
+        "itemTranslotor" : "İsmail Hakkı Akdoğan" ,
+        "itemImage":" mitoloji7.jpg",
+        "itemCreationDate":"2012",
+        "itemDescription":"<p> Egeli büyük ozan Homeros'un destanlarından İlyada bir olayın, Odysseia ise bir kişinin destanıdır. Çağdaş okurlar, İlyadadan daha sonra yaratıldığı anlaşılan Odysseiayı bir destandan çok çağdaş bir romana, bir filme benzetirler. Gerçekten de konusuyla romanı, kurgusuyla filmi andırır bu destan. Her iki destan, anlatım tekniği açısından da değişiktirler. Troya kentinin destanı olan İlyadada olaylar düz akışlı bir anlatımla sergilenirken, Odysseiada anılar, öyküler, geriye dönüşler, yer ve zaman atlamalarıyla, olay içinde olayların anlatıldığı çağdaş bir roman kurgusu görülür. İlyada, gerçek bir destandır; büyük bir olayın ve o olaya karışmış pek çok kişinin insanüstü güçlerle ilişkisini anlatır. Odysseia ise bir tek kişinin, kral Odysseus'un öyküsüdür. Troya Savaşı biteli on yıla yaklaştığı halde İthaka Kralı Odysseus, yurduna dönememiştir. Yıllardır bir adada tutukludur. Tanrılar sonunda yurduna dönmesine izin verirler. Odysseia destanı, Odysseus'un on iki gemisi ve yoldaşlarıyla yola çıkışıyla başlar, üç yıl denizlerde sürünüp bin bir tehlikeyi savuşturmasından sonra ve ancak yirmi yıl sonra yurduna ve karısına kavuşmasıyla biter. Odysseia, uygarlığımızın ilk ve belki de en ölümsüz romanıdır. <br> (Sayfa Sayısı:453) </p>"  
+    }, 
+    
+    
+    
+     {
+        "itemName":"İlyada",
+        "itemAuthor":"Homeros",
+        "itemImage":" mitoloji8.jpg",
+        "itemCreationDate":"2013",
+        "itemDescription":"<p> Troya kralı Priamos’ un oğlu Paris, Akha kralı Agamemnon’ un kardeşi Menelaos’ un karısı Helen’ i kaçırır ve Troya kentine getirir. Akhalar büyük bir ordu toplayarak Troya kentinin önüne gelirler. Agamemnon daha evvelki savaşlardan bir Apollon rahibi olan Khryses’ in kızını alır. Babasının tüm yalvarmalarına rağmen kızı vermez bunun üzerine rahip, tanrı Apollon’ dan Akhalardan öcünün alınması için yalvarır. Apollon da Akha ordusuna bir salgın salar. Çok insan ölür. En büyük Akha yiğidi olan Akhilleus’ un tavsiyesiyle düş yorumcusu Kalkhas’ ın fikrine başvurulur .O da Agamemnon’ un kızı geri vermesinden başka bir yol olmadığını söyler. Agamemnon buna çok kızar, kızı geri vermeye yanaşır ancak Akhilleus’ un armağanı olan kızı kendisine ister. Akhilleus da buna çok öfkelenir. Tanrıların araya girmesiyle Akhilleus, kızın Agamemnon’ a verilmesine razı olur ancak savaşa katılmaz ve kendi gemisinde savaşı izler. Savaş Troyalıların lehinedir. En büyük Troya yiğidi olan Hektor, Akhilleus’ un en yakın arkadaşı Patroklos’ u öldürür. Bunun üzerine çok üzülen Akhilleus savaşa katılır, Troyalıları kovalar. Hektor’ u öldürür. Ölüsünü Troya surları etrafında sürükler. Hektor’ un babası Priamos’ un yalvarmalarıyla onu babasına verir. Troya’ da on iki gün Hektor için ağıtlar yakılır ve cenaze töreni düzenlenir. <br> (Sayfa Sayısı:408) </p>"  
+    }, 
+    
+    
+    
+     {
+        "itemName":"Olympos Tanrılarının İntiharı",
+        "itemAuthor":"Haydar Ersöz",
+        "itemImage":" mitoloji9.jpg",
+        "itemCreationDate":"2002",
+        "itemDescription":"<p> Germen mitolojisinde en büyük tanrı Odin'dir! Odin, Zeus ve Marduk'a denk, savaş,cazibe, cezbe, şehvet tanrısı.Odin o kadar kahramanmış ki orduları tek başına yener, dağları bir fiske vuruşuyla devirir, ulu ağaçları bir dokunuşta kökünden sökermiş. Fakat bir ağaç varmış ki ne yapıp ne edip onu bir türlü yerinden sökemez, hatta kımıldatamazmış. Bu ağacın adı <b> Canağacı</b> imiş. Odin bütün ömrü boyunca o ağacı nasıl sökeceğinin planlarını ve provalarını yapmış. Hatta bir seferinde meyvelerinden bir tanesini kopartmaya kalkışınca gözünün biri kör olmuş, ancak, akıllanacağı yerde Odin'in hırsı daha da büyümüş. Nihayet bir gün amacına ulaşmış, Canağacı'nı sökmüş. Meğer Canağacı, Odin'in kendi canı, kendi kalbiymiş. Ve o saat, oracıkta ölmüş Odincik!İstek ve arzularını kontrol edemeyen Odin, tamahı yüzünden iki Dünya Savaşı çıkardı.Kolonyalizmi, emperyalizmi, ırkçılığı, faşizmi icad etti. İlerleme adına havayı, suyu, toprağı kirleterek, halen Canağacı'mızı sökmeye uğraşıyor. <br> <b> Odin'in hırsını kontrol altına almanın yolu nedir? Hukuk mu, erdem mi? </b> <br> (Sayfa Sayısı:384) </p>"  
+    }, 
     
     
     
@@ -20,5 +103,7 @@ var catalog = [
     
     
     
-}
+    
+    
+    
 ];
